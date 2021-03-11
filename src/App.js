b7a7f4043d5279edf5411b09539388a259cb459b
Park Jae Dong <woehd9410@gmail.com>
2021-03-11 23:55:34 +0900

    study map & jsx + props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({name, picture}){
+  return ( <div>
+    <h2>I like {name} </h2>
+    <img src={picture} alt={name} />
+  </div>
+  );
+}
+
+const foodILike = [
+  {
+    name  : "Kimchi",
+    image : "https://contents.sixshop.com/thumbnails/uploadedFiles/72878/product/image_1540176020065_1000.jpg"
+  },
+  {
+    name  : "Samgeopsal",
+    image : "asdf"
+  },
+  // {
+  //   name  : "CooKumi",
+  //   image : "sd"
+  // },
+  // {
+  //   name : "Bibimbap",
+  //   image : "df"
+  // }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodILike.map( dish =>(
+      <Food name={dish.name} picture={dish.image} />
+    ))}
+  </div>
   );
-}
+} 
 
 export default App;
